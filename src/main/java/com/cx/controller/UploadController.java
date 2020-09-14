de9e119fb06384a5d9e72bf18f545af14387fdd0
DELL <1981235781@qq.com>
2020-09-14 17:09:47 +0800

    测试1

--- src/main/java/com/cx/controller/UploadController.java
+++ src/main/java/com/cx/controller/UploadController.java
@@ -1,6 +1,8 @@
 package com.cx.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
 
     @RequestMapping("upload")
@@ -16,7 +19,7 @@ public class UploadController {
     public Object upLoadFile(@RequestParam(value = "upFile") MultipartFile multipartFile){
 
         String originalFilename = multipartFile.getOriginalFilename();
-
+        LOG.info(originalFilename);
         return originalFilename;
     }
 }
